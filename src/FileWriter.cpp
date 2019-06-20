329d78250449ccd2cbdeaaffc0f7ce7f6001ab7d
Mihkel Veske <veskem@gmail.com>
2019-06-20 16:02:07 +0300

    disable file write at write_period = 0

--- src/FileWriter.cpp
+++ src/FileWriter.cpp
@@ -22,7 +22,7 @@ void FileWriter::set_write_time() {
 
 bool FileWriter::write_time() const {
     constexpr double epsilon = 1e-10;
-    return MODES.WRITE_PERIOD >= 0 && (GLOBALS.TIME - last_write_time + epsilon) >= MODES.WRITE_PERIOD;
+    return MODES.WRITE_PERIOD > 0 && (GLOBALS.TIME - last_write_time + epsilon) >= MODES.WRITE_PERIOD;
 }
 
 bool FileWriter::first_line(ofstream &out) const {
